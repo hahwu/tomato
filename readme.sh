02598a1eb276f0dac765a8c3f07cefa242d064f4
hahwu <31872165+hahwu@users.noreply.github.com>
2017-09-13 10:45:08 +0800

    2

--- readme.sh
+++ readme.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-E_WRONG_ARGS=65
-ARGS=1
-if [[ $# -ne "$ARGS" ]]; then
-  #statements
-  echo "Usage:`basename  $0` is need one args"
-  exit $E_WRONG_ARGS
-fi
-cd 
-pwd
-chmod -R 777 xiaoyun
-echo $1
-exit 0
+#!bin/bash
+# 从/etc/fstab中读行
+File=/root/tomato/README.md
+
+{
+  read line1
+  read line2
+} < $File
+echo "First line in $File is:"
+echo "$line1"
+echo "Second line in $File is:"
+echo "$line2"
+
+exit 0;
