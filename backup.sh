e8f50bb6a9e89478a7b495e964434484afd21853
hahwu <31872165+hahwu@users.noreply.github.com>
2017-09-13 12:00:22 +0800

    1

--- backup.sh
+++ backup.sh
@@ -7,6 +7,6 @@ BACKUPFILE=backup-$(date +%m-%d-%Y)
 
 archive=${1:-$BACKUPFILE}
 
-tar cvf -`find .-mtime -1 -type f -print` > $archive.tar
+tar -cvf -`find -mtime -1 -type f -print` > $archive.tar
 gzip $archive.tar
 echo "Directory $PWD backed up in archive file \"$archive.tar.gz\"."
